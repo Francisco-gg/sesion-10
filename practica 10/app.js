d6461017fb1193fe544ef576bb5f16a5bb8263e1
Francisco-gg <franciscosampayo2503@gmail.com>
2024-12-01 09:53:12 -0500

    practicas de la 10

---
+++ practica 10/app.js
@@ -0,0 +1,14 @@
+alert("Hola Francisco");
+
+let numeroUsuario=0;
+
+let numeroSecreto=prompt("¿Podrias indicarme un numero, por favor?");
+numeroUsuario=prompt("Ingresa un numero");
+
+if(numeroSecreto==numeroUsuario){
+    alert("Acertaste el numero");
+}else{
+    alert("No acertaste el numero");
+}
+
+console.log(numeroSecreto);
