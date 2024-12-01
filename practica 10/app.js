c43836e0218aca7324241b51881a5bb5e070312c
Francisco-gg <franciscosampayo2503@gmail.com>
2024-12-01 10:05:03 -0500

    correccion al while

--- practica 10/app.js
+++ practica 10/app.js
@@ -2,10 +2,11 @@ alert("Hola Francisco");
 
 let numeroUsuario=0;
 
-let numeroSecreto=prompt("¿Podrias indicarme un numero, por favor?");
-numeroUsuario=prompt("Ingresa un numero");
-
 do{
+
+    let numeroSecreto=prompt("¿Podrias indicarme un numero, por favor?");
+    numeroUsuario=prompt("Ingresa un numero");
+
     if(numeroSecreto==numeroUsuario){
         alert("Acertaste el numero");
     }else{
