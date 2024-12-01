ab6335bdc9058cb0799e6b11d530b01fcd89ed16
Francisco-gg <franciscosampayo2503@gmail.com>
2024-12-01 12:21:40 -0500

    numerosecreto seteado

--- practica 10/app.js
+++ practica 10/app.js
@@ -1,9 +1,7 @@
 alert("Hola Francisco");
 
 let numeroUsuario=0;
-let numeroSecreto=25;
-
-Math.floor(Math.random(numeroSecreto*10+1));
+let numeroSecreto=Math.floor(Math.random()*10)+1;
 
 do{
     numeroUsuario=prompt("Ingresa un numero");
