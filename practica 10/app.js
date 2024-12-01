1ddec3bc575ee2a06b0eae302bb34fd85f386d3c
Francisco-gg <franciscosampayo2503@gmail.com>
2024-12-01 10:01:00 -0500

    do wile en js

--- practica 10/app.js
+++ practica 10/app.js
@@ -5,10 +5,12 @@ let numeroUsuario=0;
 let numeroSecreto=prompt("¿Podrias indicarme un numero, por favor?");
 numeroUsuario=prompt("Ingresa un numero");
 
-if(numeroSecreto==numeroUsuario){
-    alert("Acertaste el numero");
-}else{
-    alert("No acertaste el numero");
-}
+do{
+    if(numeroSecreto==numeroUsuario){
+        alert("Acertaste el numero");
+    }else{
+        alert("No acertaste el numero");
+    }
+}while(numeroSecreto!=numeroUsuario);
 
 console.log(numeroSecreto);
